157be89e5ebfe5adac539cbebb00626fb2d4bf2b
GLADYS <mungaiglado106@gmail.com>
2022-06-11 21:16:45 +0300

    intial commit

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solution here
+function findMatching(list, select) {
+    return list.filter(
+        (probableMatch) => probableMatch.toLowerCase() === select.toLowerCase()
+
+    );
+}function matchName(list, selectName) {
+  return list.filter((record) => record.name === selectName);
+}
+
+
+function fuzzyMatch(root, checkString) {
+    return root.filter(
+      (probableMatch) =>
+        probableMatch.toLowerCase().indexOf(checkString.toLowerCase()) === 0
+    );
+  }
